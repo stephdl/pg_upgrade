55c1e1967b98be5f58f828de1aa3a902ab684f1a
Stephane de Labrusse <stephdl@de-labrusse.fr>
2025-02-10 17:47:26 +0100

    first commit

---
+++ build-images.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#
+# Copyright (C) 2024 Nethesis S.r.l.
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+# Terminate on error
+set -e
+
+# Prepare variables for later use
+images=()
+# The image will be pushed to GitHub container registry
+repobase="${REPOBASE:-ghcr.io/stephdl}"
+# Configure the image name
+reponame="pg_upgrade"
+
+
+podman build \
+    --force-rm \
+    --layers \
+    --tag "${repobase}/postgres_upgrade" \
+    containerfile
+
+images+=("${repobase}/postgres_upgrade")
+
+
+# Commit the image
+buildah commit "${container}" "${repobase}/${reponame}"
+
+# Append the image URL to the images array
+images+=("${repobase}/${reponame}")
+
+#
+# NOTICE:
+#
+# It is possible to build and publish multiple images.
+#
+# 1. create another buildah container
+# 2. add things to it and commit it
+# 3. append the image url to the images array
+#
+
+#
+# Setup CI when pushing to Github. 
+# Warning! docker::// protocol expects lowercase letters (,,)
+if [[ -n "${CI}" ]]; then
+    # Set output value for Github Actions
+    printf "images=%s\n" "${images[*],,}" >> "${GITHUB_OUTPUT}"
+else
+    # Just print info for manual push
+    printf "Publish the images with:\n\n"
+    for image in "${images[@],,}"; do printf "  buildah push %s docker://%s:%s\n" "${image}" "${image}" "${IMAGETAG:-latest}" ; done
+    printf "\n"
+fi
